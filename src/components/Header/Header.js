f64af0b4f51d1511a82f8d9a41824e7c78bbedf0
Aarun Jassal <aaruntj@gmail.com>
2022-08-28 20:57:16 -0700

    submitted 8:57pm my time

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,7 +11,7 @@ function Header() {
           <img className="header__logo" src={headerLogo} alt="Brainflix Logo"/>
         </section>
         <section className="header__input">
-          <input type="text" className="header__search" id="" name="" placeholder="Search"/>
+          <input type="text" className="header__search" placeholder="Search"/>
           <img className="header__avatar" src={require("../../assets/images/Mohan-muruge.jpg")} alt="Avatar"/>
           <button className="header__button">
             <img className="header__button-icon" src={uploadIcon} alt="Upload"/>
--- src/components/UploadVideo/UploadVideo.js
+++ src/components/UploadVideo/UploadVideo.js
@@ -1,15 +1,33 @@
 import "./UploadVideo.scss"
+import publishIcon from "../../assets/icons/publish.svg"
 
 function UploadVideo() {
   return (
     <>
-      <h1 className="upload__container">Upload Video</h1>
-      <section>
-        <section>
-          <p>VIDEO THUMBNAIL</p>
-          <img />
-        </section>
-        <section>
+      <section className="upload">
+        <h1 className="upload__title">Upload Video</h1>
+
+        <section className="upload__container">
+          <section className="upload__image">
+            <p className="upload__image-heading">VIDEO THUMBNAIL</p>
+            <img className="upload__image-image" src={require("../../assets/images/Upload-video-preview.jpg")} alt="Upload Thumbnail"/>
+          </section>
+
+          <section className="upload__form">
+            <section className="upload__form-text">
+              <p className="upload__form-text-heading">TITLE YOUR VIDEO</p>
+              <input type="text" className="" placeholder="Add a title to your video"/>
+              <p className="upload__form-text-heading">ADD A VIDEO DESCRIPTION</p>
+              <input type="text" className="" placeholder="Add a description to your video"/>
+            </section>
+            <section className="upload__buttons">
+              <button className="upload__publish-button">
+                <img className="upload__publish-button-icon" src={publishIcon} alt="Publish Icon"/>
+                <p className="upload__publish-button-text">PUBLISH</p>
+              </button>
+              <button className="upload__cancel-button">CANCEL</button>
+            </section>
+          </section>
 
         </section>
       </section>
